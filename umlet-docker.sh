8e529d772d35a6bf4ebefc063feb95d8f6b2f034
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-04-09 15:50:05 +0200

    Add username flag

--- umlet-docker.sh
+++ umlet-docker.sh
@@ -5,11 +5,10 @@
 
 # Usage:
 # NOTE: Files need to be in current dir
-# umlet-docker.sh <input file> <output file>
-# umlet-docker.sh mahImage.uxf
+# umlet-docker.sh <username> <input file> <output file>
+# umlet-docker.sh evert mahImage.uxf mahImage.png
 
 # Runs the pdflatex container and mounts the current working dir as a vloueme of
 # the container
 
-
-exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD:/data" egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename="$1" -output="$2"
+exec docker run --name umlet-docker --rm -i --user="$(id -u $1):$(id -g $1)" --net=none -v "$PWD:/data" egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename="/data/$2" -output="/data/$3"
