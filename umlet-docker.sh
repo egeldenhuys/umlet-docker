19e05933ca675ee546947868b3d559336f11d8d5
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-04-09 15:13:56 +0200

    Control script

---
+++ umlet-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Modified from:
+# https://github.com/devng/pdflatex-docker/blob/master/pdflatex-docker.sh
+
+# Runs the pdflatex container and mounts the current working dir as a vloueme of
+# the container
+
+# abort the script if the compiled folder is not present or another error occurs
+set -e
+
+exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename "$@"
