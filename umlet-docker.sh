ab030cf2995df9e16a437c1d1c38b846cd30b7cf
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-04-09 15:31:11 +0200

    Fix filename flag

--- umlet-docker.sh
+++ umlet-docker.sh
@@ -3,10 +3,14 @@
 # Modified from:
 # https://github.com/devng/pdflatex-docker/blob/master/pdflatex-docker.sh
 
+# Usage:
+# umlet-docker.sh <fileName>
+# umlet-docker.sh mahImage.uxf
+
 # Runs the pdflatex container and mounts the current working dir as a vloueme of
 # the container
 
 # abort the script if the compiled folder is not present or another error occurs
 set -e
 
-exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename "$@"
+exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename="$1"
