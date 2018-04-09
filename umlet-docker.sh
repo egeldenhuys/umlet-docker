6e6979287f093a5b2ae79a1d34064bbaca8c292d
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-04-09 15:45:46 +0200

    Remove -e flag to continue on false error

--- umlet-docker.sh
+++ umlet-docker.sh
@@ -4,13 +4,12 @@
 # https://github.com/devng/pdflatex-docker/blob/master/pdflatex-docker.sh
 
 # Usage:
-# umlet-docker.sh <fileName>
+# NOTE: Files need to be in current dir
+# umlet-docker.sh <input file> <output file>
 # umlet-docker.sh mahImage.uxf
 
 # Runs the pdflatex container and mounts the current working dir as a vloueme of
 # the container
 
-# abort the script if the compiled folder is not present or another error occurs
-set -e
 
-exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename="$1"
+exec docker run --name umlet-docker --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD:/data" egeldenhuys/umlet-docker java -jar umlet.jar -action=convert -format=png -filename="$1" -output="$2"
